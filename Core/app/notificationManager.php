ffb7fa6ad49f19bc2632c471bb87db5237685cdd
Maël Gramain <mael@enpls.org>
2023-06-09 11:39:25 +0200

    Fix notifications

--- Core/app/notificationManager.php
+++ Core/app/notificationManager.php
@@ -3,7 +3,7 @@
 // NotificationManager.php
 // This file is used to manage notifications.
 
-if(isset($_GET["notification"])) {
+if (isset($_GET["notification"])) {
   $notification = $_GET["notification"];
   $notificationManager = new NotificationManager();
   $notificationManager->getNotification($notification);
@@ -14,120 +14,153 @@ class NotificationManager
   public function getNotification($notification): void
   {
     switch ($notification) {
-      case 'userAdded':
-      {
-        $notification = array(
-          'type' => 'success',
-          'message' => 'L\'utilisateur a bien été créé.'
-        );
-        break;
-      }
-      case 'userEdited':
-      {
-        $notification = array(
-          'type' => 'success',
-          'message' => 'L\'utilisateur a bien été modifié.'
-        );
-        break;
-      }
-      case 'userDeleted':
-      {
-        $notification = array(
-          'type' => 'success',
-          'message' => 'L\'utilisateur a bien été supprimé.'
-        );
-        break;
-      }
-      case 'userNotFound':
-      {
-        $notification = array(
-          'type' => 'danger',
-          'message' => 'L\'utilisateur n\'a pas été trouvé.'
-        );
-        break;
-      }
-      case 'login_required':
-      {
-        $notification = array(
-          'type' => 'danger',
-          'message' => 'Vous devez être connecté pour accéder à cette page.'
-        );
-        break;
-      }
-      case 'login_success':
-      {
-        $notification = array(
-          'type' => 'success',
-          'message' => 'Vous avez bien été connecté.'
-        );
-        break;
-      }
-      case 'logout_success':
-      {
-        $notification = array(
-          'type' => 'success',
-          'message' => 'Vous avez bien été déconnecté.'
-        );
-        break;
-      }
-      case 'confirm_success' :
-      {
-        $notification = array(
-          'type' => 'success',
-          'message' => 'Votre compte a bien été confirmé.'
-        );
-        break;
-      }
-      case 'already_confirmed' :
-      {
-        $notification = array(
-          'type' => 'danger',
-          'message' => 'Votre compte a déjà été confirmé.'
-        );
-        break;
-      }
-      case 'email_used':
-        {
+      case 'userAdded': {
           $notification = array(
-            'type' => 'danger',
-            'message' => 'Cet email est déjà utilisé.'
+            'type' => 'success',
+            'message' => 'L\'utilisateur a bien été ajouté.'
           );
           break;
         }
-      case 'email_invalid':
-        {
+      case 'userEdited': {
           $notification = array(
-            'type' => 'danger',
-            'message' => 'L\'email saisi est invalide.'
+            'type' => 'success',
+            'message' => 'L\'utilisateur a bien été modifié.'
           );
           break;
         }
-
-      case 'phone_invalid':
-        {
+      case 'userDeleted': {
           $notification = array(
-            'type' => 'danger',
-            'message' => 'Le numéro de téléphone saisi est invalide.'
+            'type' => 'success',
+            'message' => 'L\'utilisateur a bien été supprimé.'
           );
           break;
         }
-
-      case 'password_mail_sent' :
-      {
-        $notification = array(
-          'type' => 'success',
-          'message' => 'Un email vous a été envoyé pour réinitialiser votre mot de passe.'
-        );
-        break;
-      }
-      default :
-      {
-        $notification = array(
-          'type' => 'danger',
-          'message' => 'Une erreur est survenue.'
-        );
-        break;
-      }
+      case 'userPaid': {
+          $notification = array(
+            'type' => 'success',
+            'message' => 'L\'utilisateur a payé.'
+          );
+          break;
+        }
+      case 'userNotFound': {
+          $notification = array(
+            'type' => 'danger',
+            'message' => 'L\'utilisateur n\'a pas été trouvé.'
+          );
+          break;
+        }
+      case 'entryDeleted': {
+          $notification = array(
+            'type' => 'success',
+            'message' => 'L\'entrée a bien été supprimée.'
+          );
+          break;
+        }
+      case 'entryEdited': {
+          $notification = array(
+            'type' => 'success',
+            'message' => 'L\'entrée a bien été modifiée.'
+          );
+          break;
+        }
+      case 'tokenNotFound': {
+          $notification = array(
+            'type' => 'danger',
+            'message' => 'Le token n\'a pas été trouvé.'
+          );
+          break;
+        }
+      case 'tokenAlreadyUsed': {
+          $notification = array(
+            'type' => 'danger',
+            'message' => 'Le token a déjà été utilisé.'
+          );
+          break;
+        }
+      case 'tokenSuccess': {
+          $notification = array(
+            'type' => 'success',
+            'message' => 'Le token a fonctionné.'
+          );
+          break;
+        }
+      case 'invalidPassword': {
+          $notification = array(
+            'type' => 'danger',
+            'message' => 'Le mot de passe est invalide.'
+          );
+          break;
+        }
+      case 'profilUpdated': {
+          $notification = array(
+            'type' => 'success',
+            'message' => 'Le profil a bien été mis à jour.'
+          );
+          break;
+        }
+      case 'login_required': {
+          $notification = array(
+            'type' => 'danger',
+            'message' => 'Vous devez être connecté pour accéder à cette page.'
+          );
+          break;
+        }
+      case 'permission_denied': {
+          $notification = array(
+            'type' => 'danger',
+            'message' => 'Vous n\'avez pas les permissions pour accéder à cette page.'
+          );
+          break;
+        }
+      case 'logout_success': {
+          $notification = array(
+            'type' => 'success',
+            'message' => 'Vous avez bien été déconnecté.'
+          );
+          break;
+        }
+      case 'login_success': {
+          $notification = array(
+            'type' => 'success',
+            'message' => 'Vous avez bien été connecté.'
+          );
+          break;
+        }
+      case 'forgot_password': {
+          $notification = array(
+            'type' => 'success',
+            'message' => 'Un email vous a été envoyé.'
+          );
+          break;
+        }
+      case 'register_success': {
+          $notification = array(
+            'type' => 'success',
+            'message' => 'Vous avez bien été enregistré.'
+          );
+          break;
+        }
+      case 'new_password': {
+          $notification = array(
+            'type' => 'success',
+            'message' => 'Votre mot de passe a bien été modifié.'
+          );
+          break;
+        }
+      case 'contact_sent': {
+          $notification = array(
+            'type' => 'success',
+            'message' => 'Votre message a bien été envoyé.'
+          );
+          break;
+        }
+      default: {
+          $notification = array(
+            'type' => 'danger',
+            'message' => 'Une erreur est survenue.'
+          );
+          break;
+        }
     }
     global $smarty;
     $smarty->assign('notification', $notification);
